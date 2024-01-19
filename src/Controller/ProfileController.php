ce58086ae28bad16e7bfa9e937c9839d8c284923
OnaFlorent <bflorent53170@gmail.com>
2024-01-19 14:38:34 +0100

    add all CRUD for profile feature

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\EditProfileFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route(path: '/profil', name: 'profile_')]
 class ProfileController extends AbstractController
@@ -12,8 +15,29 @@ class ProfileController extends AbstractController
     #[Route(path: '/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('profile/index.html.twig', [
-            'controller_name' => 'Profil de l\'utilisateur',
+        return $this->render('profile/index.html.twig');
+    }
+
+    #[Route(path: '/modifierprofil', name: 'edit_profile')]
+    public function editProfile(Request $request, EntityManagerInterface $emi)
+    {
+        $user =$this->getUser();
+        $form = $this->createForm(EditProfileFormType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $emi->persist($user);
+            $emi->flush();
+
+            $this->addFlash('success', 'Profil mis à jour avec succès');
+            return $this->redirectToRoute('profile_edit_profile');
+        }
+        
+        return $this->render('profile/edit_profile.html.twig', [
+            'editProfileFormType' => $form->createView()
         ]);
     }
+
+    
 }
